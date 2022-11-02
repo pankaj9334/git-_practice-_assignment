8bd13452c39a1c884f3e1244b3dd25353c91dc62
pankaj9334 <pankaj933426@gmail.com>
2022-11-02 12:07:23 +0530

    prime folder created

---
+++ prime.js
@@ -0,0 +1,17 @@
+function primecheck(num){
+
+for(i=0;i<=num.length-1;i++){
+
+ count++;
+
+}
+
+return true;
+}
+return false;
+}
+let ans=primecheck(23);
+if (ans==true){
+console.log("Prime Number");
+}else{
+ console.log("Not a Prime Number");
